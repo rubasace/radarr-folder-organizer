d54f8bf2a5d8ce82855df7fd01eb5785d159c285
rubenpve <rverdugo@travix.com>
2021-08-18 19:25:02 +0200

    Fix syntax and make sure individual movie errors don't break the whole run

--- FolderOrganizer.py
+++ FolderOrganizer.py
@@ -41,25 +41,28 @@ def config_section_map(section):
             dict1[option] = None
     return dict1
 
+
 def get_v2_custom_format_names(movie_info):
     return list(map(lambda e: e["name"], movie_info["movieFile"]["quality"]["customFormats"]))
 
-def get_custom_format_names(movie_info, isLegacyV2):
+
+def get_custom_format_names(movie_info, is_legacy_v2):
     if "movieFile" not in movie_info.keys():
         return list()
-    if isLegacyV2:
+    if is_legacy_v2:
         return get_v2_custom_format_names(movie_info)
-    movieFileId = movie_info["movieFile"]["id"]
-    movieFileResponse = radarrSession.get('{0}/api/v3/movieFile/{1}'.format(radarr_url, movieFileId))
-    if movieFileResponse.status_code >= 300:
-        logger.error('Movie file retrieval for movie {} returned status code {}'.format(movie_info['title'], radarrMovies.status_code))
-        sys.exit(0)
-    movieFile = movieFileResponse.json()
-    return list(map(lambda e: e["name"], movieFile["customFormats"]))
+    movie_file_id = movie_info["movieFile"]["id"]
+    movie_file_response = radarr_session.get('{0}/api/v3/movieFile/{1}'.format(radarr_url, movie_file_id))
+    if movie_file_response.status_code >= 300:
+        logger.error('Movie file retrieval for movie {} returned status code {}'.format(movie_info['title'],
+                                                                                        radarr_movies.status_code))
+        return list()
+    movie_file = movie_file_response.json()
+    return list(map(lambda e: e["name"], movie_file["customFormats"]))
 
 
-def decide_path(movie_info, format_mappings, isLegacyV2):
-    movie_custom_formats = get_custom_format_names(movie_info, isLegacyV2)
+def decide_path(movie_info, format_mappings, is_legacy_v2):
+    movie_custom_formats = get_custom_format_names(movie_info, is_legacy_v2)
     movie_title = movie_info['title']
     logger.debug('Movie "{}" has custom formats: {}'.format(movie_title, movie_custom_formats))
     if len(movie_custom_formats) > 0:
@@ -99,13 +102,13 @@ def normalize_path(path):
     return os.path.normcase(normpath)
 
 
-def move_movie(movie_info, current_path, correct_path):
+def move_movie(movie_info, current, new):
     movie_title = movie_info["title"]
     old_path = movie_info[PATH]
-    new_path = old_path.replace(current_path, correct_path).rstrip("/\\")
+    new_path = old_path.replace(current, new).rstrip("/\\")
 
     from_folder_name = movie_info[FOLDER_NAME]
-    new_folder_name = from_folder_name.replace(current_path, correct_path).rstrip("/\\")
+    new_folder_name = from_folder_name.replace(current, new).rstrip("/\\")
     try:
         if 'movieFile' in movie_info.keys():
             logger.debug('Trying to move movie "{}" files from "{}" to "{}"'.format(title, old_path, new_path))
@@ -127,8 +130,8 @@ def change_movie_path_and_folder(movie_info, new_path, new_folder_name):
     old_path = movie_info[PATH]
     movie_info[PATH] = new_path
     movie_info[FOLDER_NAME] = new_folder_name
-    update_response = radarrSession.put('{0}/api/movie/{1}'.format(radarr_url, movie_id),
-                                        data=json.dumps(movie_info))
+    update_response = radarr_session.put('{0}/api/movie/{1}'.format(radarr_url, movie_id),
+                                         data=json.dumps(movie_info))
     movie_title = movie_info["title"]
     if update_response.status_code < 300:
         logger.debug(
@@ -146,8 +149,8 @@ def refresh_movie(movie_info):
         "Waiting {}s before refreshing movie {}, so Radarr has enough time to get the change".format(sleep_time,
                                                                                                      movie_title))
     time.sleep(sleep_time)
-    refresh_response = radarrSession.post("{0}/api/command".format(radarr_url),
-                                          data=json.dumps(command))
+    refresh_response = radarr_session.post("{0}/api/command".format(radarr_url),
+                                           data=json.dumps(command))
     if refresh_response.status_code < 300:
         logger.debug("Movie {} refreshed succesfully".format(movie_title))
     else:
@@ -192,56 +195,66 @@ custom_format_mappings = {key: normalize_path(value) for (key, value) in
                           config_section_map("CustomFormatMappings").items()}
 if DEFAULT_MAPPING not in custom_format_mappings:
     logger.error('A default mapping named {} should be provided!!'.format(DEFAULT_MAPPING))
-    sys.exit(0)
+    sys.exit(1)
 
-radarrSession = requests.Session()
-radarrSession.headers.update({'x-api-key': radarr_key})
+radarr_session = requests.Session()
+radarr_session.headers.update({'x-api-key': radarr_key})
 # TODO check if needed
-radarrSession.trust_env = False
-radarrStatus = radarrSession.get('{0}/api/system/status'.format(radarr_url))
-status_json = radarrStatus.json()
+radarr_session.trust_env = False
+radarr_status = radarr_session.get('{0}/api/system/status'.format(radarr_url))
+status_json = radarr_status.json()
 version = status_json['version']
 logger.info('Connected to radarr at version {}'.format(version))
 isLegacyV2 = version[0] == '2'
-if radarrStatus.status_code >= 300:
-    logger.error('Status retrieve returned status code {}'.format(radarrStatus.status_code))
-    sys.exit(0)
-radarrMovies = radarrSession.get('{0}/api/movie'.format(radarr_url))
-if radarrMovies.status_code >= 300:
-    logger.error('Movies retrieve returned status code {}'.format(radarrMovies.status_code))
-    sys.exit(0)
+if radarr_status.status_code >= 300:
+    logger.error('Status retrieve returned status code {}'.format(radarr_status.status_code))
+    sys.exit(2)
+radarr_movies = radarr_session.get('{0}/api/movie'.format(radarr_url))
+if radarr_movies.status_code >= 300:
+    logger.error('Movies retrieve returned status code {}'.format(radarr_movies.status_code))
+    sys.exit(3)
 
-movies_json = radarrMovies.json()
+movies_json = radarr_movies.json()
 logger.info('Received {} movies from Radarr'.format(len(movies_json)))
 should_move_default = os.getenv(MOVE_DEFAULT_FLAG, 'false').lower() == 'true'
+errors_happened = False
 for movie in movies_json:
-    title = movie["title"]
-    logger.debug('########## Started processing movie: {} ##########'.format(title))
-    current_path = get_current_path(movie)
-    # We normalize the path to avoid case inconsistencies when checking the mappings
-    normalized_current_path = normalize_path(current_path)
-    if normalized_current_path not in custom_format_mappings.values():
-        logger.warning(
-            'Movie "{}" current path is "{}", normalized as "{}" and is not in the configuration file. Skipping to avoid possible errors'.format(
-                title, movie['path'], normalized_current_path))
-        continue
-    correct_path = decide_path(movie, custom_format_mappings, isLegacyV2)
-    if correct_path is None:
-        logger.debug(
-            'Movie "{}" current path is "{}", correct path is not assigned (no custom format with a customized mapping). Skipping'.format(
-                title, movie['path']))
-        continue
-    if normalized_current_path != correct_path:
-        logger.debug(
-            'Movie "{}" current path is "{}", normalized as "{}" and doesn\'t match the correct path "{}". Proceeding to move it'.format(
-                title, movie['path'], normalized_current_path, correct_path))
-        move_movie(movie, current_path, correct_path)
-    else:
-        logger.debug(
-            'Movie "{}" current path is "{}" and matches the correct path "{}". Nothing to do here'.format(
-                title,
-                movie['path'],
-                correct_path))
-    logger.debug('########## Finished processing movie: {} ##########'.format(title))
+    try:
+        title = movie["title"]
+        logger.debug('########## Started processing movie: {} ##########'.format(title))
+        current_path = get_current_path(movie)
+        # We normalize the path to avoid case inconsistencies when checking the mappings
+        normalized_current_path = normalize_path(current_path)
+        if normalized_current_path not in custom_format_mappings.values():
+            logger.warning(
+                'Movie "{}" current path is "{}", normalized as "{}" and is not in the configuration file. Skipping to avoid possible errors'.format(
+                    title, movie['path'], normalized_current_path))
+            continue
+        correct_path = decide_path(movie, custom_format_mappings, isLegacyV2)
+        if correct_path is None:
+            logger.debug(
+                'Movie "{}" current path is "{}", correct path is not assigned (no custom format with a customized mapping). Skipping'.format(
+                    title, movie['path']))
+            continue
+        if normalized_current_path != correct_path:
+            logger.debug(
+                'Movie "{}" current path is "{}", normalized as "{}" and doesn\'t match the correct path "{}". Proceeding to move it'.format(
+                    title, movie['path'], normalized_current_path, correct_path))
+            move_movie(movie, current_path, correct_path)
+        else:
+            logger.debug(
+                'Movie "{}" current path is "{}" and matches the correct path "{}". Nothing to do here'.format(
+                    title,
+                    movie['path'],
+                    correct_path))
+        logger.debug('########## Finished processing movie: {} ##########'.format(title))
+    except Exception as e:
+        logger.error('An error happened while processing movie {}: {}'.format(movie["title"], repr(e)))
+        logger.exception(e)
+        errors_happened = True
 
-logger.info("Done!!")
+if errors_happened:
+    logger.info("Done with errors!!")
+    sys.exit(4)
+else:
+    logger.info("Done!!")
